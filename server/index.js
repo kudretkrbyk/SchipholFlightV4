873725c4adb3cfe5a7c8af54bee2cdeccd7bcbd8
kudretkrbyk <kudretkrbyk@gmail.com>
2024-09-17 19:57:33 +0300

    added APİ GET

--- server/index.js
+++ server/index.js
@@ -1,20 +1,33 @@
 const express = require("express");
+const axios = require("axios");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Basit bir test rotası
-app.get("/", (req, res) => {
-  res.send("Node.js Sunucusu Çalışıyor!");
+app.get("/flights", async (req, res) => {
+  const url =
+    "https://api.schiphol.nl/public-flights/flights?includedelays=false&page=0&sort=%2BscheduleTime";
+
+  const headers = {
+    Accept: "application/json",
+    app_id: "88d84de8",
+    app_key: "bc36d80270d7dd258f0c16084d8f89df",
+    ResourceVersion: "v4",
+  };
+
+  try {
+    const response = await axios.get(url, { headers });
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Bir hata oluştu.");
+  }
 });
 
-// Sunucuyu başlatma
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda çalışıyor.`);
 });
